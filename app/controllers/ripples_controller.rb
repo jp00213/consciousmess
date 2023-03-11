9d9e0c0a7122c5fc2a5217813386926035151d39
Jennifer Parks <jp00213@my.westga.edu>
2023-03-11 14:47:18 -0500

    Adjusted CSS for showing message. Added truncated message option on showing list. New ripple redirects to index now.

--- app/controllers/ripples_controller.rb
+++ app/controllers/ripples_controller.rb
@@ -25,8 +25,7 @@ class RipplesController < ApplicationController
 
     respond_to do |format|
       if @ripple.save
-        format.html { redirect_to ripple_url(@ripple), notice: "Ripple was successfully created." }
-        format.json { render :show, status: :created, location: @ripple }
+        format.html { redirect_to action: "index" }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @ripple.errors, status: :unprocessable_entity }
